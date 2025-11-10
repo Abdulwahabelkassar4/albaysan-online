8abf0a8613ce53a50684af3a6045c013f9ac05a2
Abdulwahab <abdulwahabelkassar4@gmail.com>
2025-11-10 19:31:18 +0300

    Allow Render host in Vite preview config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+  preview: {
+    allowedHosts: ['albaysan-onlinefrontend.onrender.com'],
+    host: '0.0.0.0',
+    port: 10000
+  }
+})
 
